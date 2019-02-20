09a630c2582526945ae5ea5738252402f1d5f138
ali-718 <alimurtuza718@gmail.com>
2019-02-20 07:53:24 -0800

    Data section remaining

--- components/nav.js
+++ components/nav.js
@@ -2,8 +2,29 @@ import React, { Component } from 'react';
 import { Container, Header, Tab, Tabs, TabHeading, Icon, Text, Footer,Body,Title } from 'native-base';
 import Sample from './content';
 import {View, StyleSheet} from 'react-native';
+import { BottomNavigation} from 'react-native-paper';
+
+const FieldsTab = () => <Sample/>;
+
+const DataTab = () => <Text>Albums</Text>;
+
 
 export default class Nav extends Component {
+  state = {
+    index: 0,
+    routes: [
+      { key: 'fields', icon: 'add-circle-outline' },
+      { key: 'data', icon: 'supervisor-account' },
+    ],
+  }
+
+  _handleIndexChange = index => this.setState({ index });
+
+  _renderScene = BottomNavigation.SceneMap({
+    fields: FieldsTab,
+    data: DataTab,
+  });
+
   render() {
     return (
       <Container>
@@ -13,17 +34,12 @@ export default class Nav extends Component {
           </Body>
         </Header>
 
-        <Tabs >
-          <Tab heading={ <TabHeading><Icon name="camera" /><Text>Camera</Text></TabHeading>}>
-            <Sample />
-          </Tab>
-          <Tab heading={ <TabHeading><Text>No Icon</Text></TabHeading>}>
-            <Text>Hello</Text>
-          </Tab>
-          <Tab heading={ <TabHeading><Icon name="apps" /></TabHeading>}>
-            <Text>Ali</Text>
-          </Tab>
-        </Tabs>
+      <BottomNavigation
+        navigationState={this.state}
+        onIndexChange={this._handleIndexChange}
+        renderScene={this._renderScene}
+        shifting={true}
+      />
       </Container>
     );
   }
